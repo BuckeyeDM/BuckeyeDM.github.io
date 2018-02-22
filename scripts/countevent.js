9d717a244c9a8d5d2f3f9a19af7db4e7e986b277
BuckeyeDM <AlbertLeungPRC@gmail.com>
2018-02-22 11:39:18 -0500

    update

--- scripts/countevent.js
+++ scripts/countevent.js
@@ -1,6 +1,6 @@
 if (typeof jQuery != 'undefined') {
     jQuery(document).ready(function($) {
-        var filetypes = /\.(zip|exe|pdf|doc*|xls*|ppt*|mp3)$/i;
+        var filetypes = /\.(zip|txt|bib|exe|pdf|doc*|xls*|ppt*|mp3)$/i;
         var baseHref = '';
         if (jQuery('base').attr('href') != undefined)
             baseHref = jQuery('base').attr('href');
@@ -9,7 +9,7 @@ if (typeof jQuery != 'undefined') {
             if (href && (href.match(/^https?\:/i)) && (!href.match(document.domain))) {
                 jQuery(this).click(function() {
                     var extLink = href.replace(/^https?\:\/\//i, '');
-                    _gaq.push(['_trackEvent', 'External', 'Click', extLink]);
+                    ga('send', 'event', 'External', 'Click', extLink);
                     if (jQuery(this).attr('target') != undefined && jQuery(this).attr('target').toLowerCase() != '_blank') {
                         setTimeout(function() { location.href = href; }, 200);
                         return false;
@@ -19,14 +19,14 @@ if (typeof jQuery != 'undefined') {
             else if (href && href.match(/^mailto\:/i)) {
                 jQuery(this).click(function() {
                     var mailLink = href.replace(/^mailto\:/i, '');
-                    _gaq.push(['_trackEvent', 'Email', 'Click', mailLink]);
+                    ga('send', 'event', 'Email', 'Click', mailLink);
                 });
             }
             else if (href && href.match(filetypes)) {
                 jQuery(this).click(function() {
                     var extension = (/[.]/.exec(href)) ? /[^.]+$/.exec(href) : undefined;
                     var filePath = href;
-                    _gaq.push(['_trackEvent', 'Download', 'Click-' + extension, filePath]);
+                    ga('send', 'event', 'Download', 'Click-' + extension, filePath);
                     if (jQuery(this).attr('target') != undefined && jQuery(this).attr('target').toLowerCase() != '_blank') {
                         setTimeout(function() { location.href = baseHref + href; }, 200);
                         return false;
